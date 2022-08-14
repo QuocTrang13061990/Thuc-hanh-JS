9aa6063f958711f5a0a0655eee92832af527a150
QuocTrang13061990 <tqtwip@gmail.com>
2022-08-14 22:40:47 +0700

    #1 - validate form (buổi 1) - chưa hoàn thành

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Thuc hanh JS
 Học theo: Lab1 Bai1 WEB2061 - Lập trình Javascrift nâng cao - SP21
+
+# Learn JS 
+    - website: https://www.javascripttutorial.net
+        + Validate form: 
+            + https://www.javascripttutorial.net/javascript-dom/javascript-form-validation/#:~:text=What%20is%20form%20validation,is%20called%20client%2Dside%20validation.
+        + Regex thì xem tại: https://www.javascripttutorial.net/javascript-regular-expression/
+
+
+# Note
+    1. Các cách viết function trong javascript
+        - function isRequired(){}
+        - const isRequired = function(){}
+        - ES6: 
+            - const isRequired = param => logic
+            - const isRequired = (param) => logic 
+            - const isRequired = param => {logic}  
+            - EX: 
+                - const isRequired = value => value === '' ? false : true;
+                - const isRequired = (value) => value === '' ? false : true;
+                - const isRequired = value => { return value === '' ? false : true;}
+    2. remove, add Class ()
+        - Example
+            - formField.classList.remove('success');
+            - formField.classList.add('error');
+    3. Viết biến trong chuỗi
+        - showError(usernameEl, `Username must be between ${min} and ${max} characters.`)
+    4. CSS
+        - Hiển thị border error của thẻ input (form-field nào có class = "error" thì thiết lập border cho thẻ input)
+            - .form-field.error input {border-color: var(--error-color);
+                                                }
---
+++ form-validation/js/app.js
@@ -0,0 +1,111 @@
+const usernameEl = document.getElementById('username');
+const emailEl = document.getElementById('email');
+const passwordEl = document.getElementById('password');
+const confirmPasswordEl = document.getElementById('confirm-password');
+
+const form = document.getElementById('signup');
+
+// console.log(usernameEl, emailEl, passwordEl, confirmPasswordEl, form);
+const isRequired = value => value === '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email); // kiểm tra email có đúng định dạng re hay không
+}
+const isPasswordSecure = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+};
+// input la mot trong cac input: userNameEl, emailEl, passwordEl, confirmPasswordEl
+const showError = (input, message) => {
+    const formField = input.parentElement;
+    formField.classList.remove('success');
+    formField.classList.add('error');
+    const error = formField.querySelector('small');
+    error.textContent = message;
+}
+const showSuccess = (input) => {
+    const formField = input.parentElement;
+    formField.classList.remove('error');
+    formField.classList.add('success');
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
+// 1. Validate the username field: không trống, 3=<length<=25
+const checkUsername = () => {
+    let valid = false;
+    const min = 3, max = 25;
+    // lấy value của field input (trim() giống như bên PHP)
+    const username = usernameEl.value.trim();
+    if (!isRequired(username)) {
+        showError(usernameEl, 'Username cannot be blank');
+    } else if (username.length < min || username.length > 25) {
+        showError(usernameEl, `Username must be between ${min} and ${max} characters.`);
+    } else {
+        showSuccess(usernameEl);
+        valid = true;
+    }
+    return valid;
+}
+// 2. Validate the email field
+const checkEmail = () => {
+    let valid = false;
+    // lấy value của field input (trim() giống như bên PHP)
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+}
+// 3. Validate the password field
+const checkPassword = () => {
+    let valid = false;
+    // lấy value của field input (trim() giống như bên PHP)
+    const password = passwordEl.value.trim();
+    if (!isRequired(password)) {
+        showError(passwordEl, 'Password cannot be blank');
+    } else if (!isPasswordSecure(password)) {
+        showError(passwordEl, 'Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)')
+    } else {
+        showSuccess(passwordEl);
+        valid = true;
+    }
+    return valid;
+}
+// 4. Validate the confirm-password field
+const checkConfirmPassword = () => {
+    let valid = false;
+    // lấy value của field input (trim() giống như bên PHP)
+    const confirmPassword = confirmPasswordEl.value.trim();
+    const password = passwordEl.value.trim();
+
+    if (!isRequired(confirmPassword)) {
+        showError(confirmPasswordEl, 'Please enter the password again');
+    } else if (confirmPassword !== password) {
+        showError(confirmPasswordEl, 'Confirm password does not match')
+    } else {
+        showSuccess(confirmPasswordEl);
+        valid = true;
+    }
+    return valid;
+}
+// Click vào bất kỳ đâu trên form
+form.addEventListener('click', function (e) {
+    // prevent the form from submitting
+    e.preventDefault;
+    // lưu ý  biến isEmailValid dưới đây khác với isEmailValid() ở trên (dù trùng tên nhưng không thành vấn đề)
+    let isUsernameValid = checkUsername(), isEmailValid = checkEmail(), isPasswordValid = checkPassword(), isConfirmPasswordValid = checkConfirmPassword();
+    let isFormValid = isUsernameValid && isEmailValid && isPasswordValid && isConfirmPasswordValid;
+    // submit to the server if the form is valid (phần gửi data cho server không được đề cập ở đây => để trống)
+    if (isFormValid) {
+
+    }
+})
+
+// showSuccess(usernameEl);
+// console.log(isEmailValid("tqtwip@gmail.com"));
